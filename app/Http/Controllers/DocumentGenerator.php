861c9eb2cd2ef703819f74ba59b390e605ee22ed
Joseph M.D <domguiajm@yahoo.fr>
2018-01-26 19:32:56 +0100

    adding data to the pdf

--- app/Http/Controllers/DocumentGenerator.php
+++ app/Http/Controllers/DocumentGenerator.php
@@ -23,15 +23,15 @@ class DocumentGenerator extends Controller
         
         $contains = '<table class="maintable" width="100%">';
         foreach ($docs as $key => $doc) {
-            if($key%2) $contains .= '<tr>'; 
+            if($key%2==0) $contains .= '<tr>'; 
             $contains .= $this->buildBlock($doc);
-            if($key%2) $contains .= '</tr>';
+            if($key%2==1) $contains .= '</tr>';
         }
         $contains .= '</table>';
-
+        
         $style = '<style>
         table td{font-size: 1em;}
-        .maintable{border: 1px solid #CCC; border-spacing: 15px;}
+        .maintable{border: none; border-spacing: 15px;}
         .block{padding: 10px; border: 1px solid #000;}
         </style>';
         $html = '<html><body>'.$style.$header.$contains.'</body></html>';
@@ -49,16 +49,18 @@ class DocumentGenerator extends Controller
         return $dompdf->stream('Fiches Catalographiques.pdf',array('Attachment'=>0));
     }
 
-    private function buildBlock($data){
+    private function buildBlock($doc){
 
-    $block = <<<'ENDHTML'
+    $block = <<<"ENDHTML"
     <td class='block'>
             <table>
             <tr>
-                <td>Titre, auteur, année de publication<br><br><td>
+                <td>$doc->TitreDocuments, $doc->Auteur, $doc->AnneePublicationDocuments<br><br><td>
             </tr>
             <tr>
-                <td>Cote<br><br><td>
+                <td>
+                $doc->Section<br>
+                $doc->CoteDocuments<br><td>
             </tr>
             </table>
     </td>
