3f8f30b3d69da5d818986cd905fce5962907a78b
Joseph M.D <domguiajm@yahoo.fr>
2018-02-08 12:27:36 +0000

    improve view

--- app/Http/Controllers/DocumentGenerator.php
+++ app/Http/Controllers/DocumentGenerator.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Dompdf\Dompdf;
 use App\Documents;
 
@@ -27,7 +28,7 @@ class DocumentGenerator extends Controller
         
         $contains = '<table class="maintable" width="100%">';
         $contains .= '<tr>';
-        for($i=0;$i<200;$i++) {
+        for($i=0;$i<20;$i++) {
         //for($i=0; $i<count($docs); $i++) {
             $contains .= $this->buildBlock($docs[$i%count($docs)], $i);
             if($i>0 && $i%2==1) $contains .= '</tr><tr>';
@@ -38,9 +39,10 @@ class DocumentGenerator extends Controller
         
         $style = '<style>
         html, body, table {margin:0;}
-        .maintable td{ font-size: 1.2em; }
+        .maintable td{ font-size: 1.05em; }
         .maintable{ table-layout: fixed; border: none; border-spacing: 0px;}
         .block{ overflow:hidden; line-height:2; width:125mm; height:88.5mm; page-break-inside: avoid; border: 1px dashed #999;}
+        .lowline{ line-height:0.98em }
         </style>';
         $html = '<html><head>'.$style.'</head><body>'.$header.$contains.'</body></html>';
 
@@ -66,39 +68,74 @@ class DocumentGenerator extends Controller
             $sousdomaine     = $doc->SousDomaines->NomSousDomaines;
         }
 
+        $injectIf = function ($data, $prefix=' ', $suffix=', '){
+            if($data){
+                return $prefix.trim($data).$suffix;
+            }
+            return '';
+        };
+
         $displayHack = '';// ($i==7)?'style="page-break-after:always;background:red;height:50mm;"':'';
 
-        $block = <<<"ENDHTML"
+        $storagePath  = Storage::disk('local')->getDriver()->getAdapter()->getPathPrefix(); 
+        $logoPath = $storagePath.'public/logoenam.jpg';
+        
+        $block = "
         <td class='block' valign='top' $displayHack>
             <div style='position:relative; width:100%; height:87.5mm; overflow:hidden; padding: 30px;'>
                 <table style='position:absolute;' width='100%' height='100%'>
                     <tr>
-                        <td style='text-align:left;'>
-                            $doc->CoteDocuments
+                        <td style='text-align:left;' width='33%'>
+                            <strong>$doc->CoteDocuments</strong>
+                        </td>
+                        <td style='text-align:center;font-size:0.8em;' width='33%'>
+                            <img src='$logoPath' width=50><br>
+                            <em>Bibliothèque de l'ENAM</em>
+                        </td>
+                        <td style='text-align:right;' width='33%'>
+                            <strong>$domaine</strong>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan='3'>
+                            <br>
+                            <strong>$doc->TitreDocuments</strong> / $doc->Auteur 
+                            ".$injectIf($doc->LieuEditionDocuments, ' - ', '')."
+                            ".$injectIf($doc->AnneePublicationDocuments, ', ','')."
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan='3' class='lowline'>
+                            ".$injectIf($doc->EditionsDocuments)."
+                            ".$injectIf($doc->AnneeEditionDocuments)."
+                            ".$injectIf($doc->MaisonEditionDocuments)."
+                            ".$injectIf($doc->LongueurEditionDocuments, ' ', 'cm, ')."
+                            ".$injectIf($doc->AdresseMaisonEdition)."
+                            ".$injectIf($doc->IllustrationDocuments)."
+                            ".$injectIf($doc->ReliureDocuments, ' ', '.')."
+                            ".$injectIf($doc->NumeroEntresDocuments, ' - N° ')."
+                            ".$injectIf($doc->PeriodiciteDocuments, ' - ')."
+                        </td>
+                    </tr>
+                    <tr><td><br></td></tr>
+                    <tr style='vertical-align:top;'>
+                        <td colspan='2' class='lowline'>
+                            ".$injectIf($doc->IsbnDocuments, 'ISBN: ', '<br>')."
+                            ".$injectIf($doc->IssnDocuments, 'ISSN: ', '')."
                         </td>
                         <td style='text-align:right;'>
-                        $domaine
+                            $doc->NbreExemplaireEdition exemplaire(s)<br>
                         </td>
                     </tr>
+
                     <tr>
-                        <td>$doc->Section</td>
-                    </tr>
-                    <tr>
-                        <td>$doc->TitreDocuments</td>
-                    </tr>
-                    <tr>
-                        <td>$doc->Auteur, $doc->AnneePublicationDocuments</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td style='text-align:right;'>
-                            $sousdomaine
+                        <td colspan='3' style='text-align:right;'>
+                            <strong>$sousdomaine</strong>
                         </td>
                     </tr>
                 </table>
             </div>
-        </td>
-ENDHTML;
+        </td>";
 
         return $block;
     }
